84e48d239237077656d9dc693d9ce1764a2719a9
Jeff Bryner <jeff@jeffbryner.com>
2026-05-31 14:58:08 -0700

    phase three: Please enter the commit message for your changes. Lines starting

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,9 +5,9 @@ import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  authDomain: "prj-aigenda-adf.firebaseapp.com",
+  projectId: "prj-aigenda-adf",
+  storageBucket: "prj-aigenda-adf.firebasestorage.app",
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
